e1aabd5892bd81af9e74438ee075cc1bf75258b6
Mike Bernard <mike.bernard@uconn.edu>
2021-08-04 18:18:33 -0700

    add UnitQuaternion class and tests

--- python_quaternions/quaternion.py
+++ python_quaternions/quaternion.py
@@ -2,6 +2,8 @@ import numpy as np
 
 
 class Quaternion:
+    """A base quaternion class with universal quaternion properties."""
+
     def __init__(self, w: float, x: float, y: float, z: float) -> None:
         """
         Instantiate a quaternion from its component parts.
@@ -12,13 +14,21 @@ class Quaternion:
             y (float): second vector component
             z (float): third vector component
         """
-        self.w = w
-        self.v = np.array([x, y, z])
+        self.__scalar = w
+        self.__vector = np.array([x, y, z])
 
     @property
     def array(self):
         """Get the quaternion as a scalar-first array."""
         return np.array([self.w, self.x, self.y, self.z])
+
+    @property
+    def w(self):
+        return self.__scalar
+
+    @property
+    def v(self):
+        return self.__vector
 
     @property
     def x(self):
@@ -37,7 +47,47 @@ class Quaternion:
         mag = np.linalg.norm(self.array)
         if mag > 0:
             arr_norm = self.array/mag
-            self.w = arr_norm[0]
-            self.v = arr_norm[1:]
+            self.__scalar = arr_norm[0]
+            self.__vector = arr_norm[1:]
         else:
-            raise ValueError('Cannot normalize quaternion with magnitude less than 0')
+            raise ValueError(('Cannot normalize quaternion with non-positive '
+                              'magnitude.'))
+
+
+class UnitQuaternion(Quaternion):
+    """A quaternion class that enforces unit magnitude."""
+
+    def __init__(self, w: float, x: float, y: float, z: float, tol: float = 1e-5) -> None:
+        """
+        Create the quaternion with the closest floating point values to the
+        inputted components such that the magnitude is as close to 1.0 as
+        possible.
+
+        Args:
+            w (float): scalar component
+            x (float): first vector component
+            y (float): second vector component
+            z (float): third vector component
+            tol (float): if the difference between 1.0 and the norm of the
+                inputted components is greater than tol, raise an Exception
+        """
+        norm = np.linalg.norm([w, x, y, z])
+        if norm == 0:
+            raise ValueError(('Cannot create UnitQuaternion from components with '
+                              'zero magnitude.'))
+        if np.abs(1.0 - norm) > tol:
+            raise ValueError(('The magnitude of the inputted components is '
+                              'greater than the tolerance for rounding to 1.'))
+
+        w, x, y, z = np.array([w, x, y, z])/norm
+        super().__init__(w, x, y, z)
+
+        self.__tol = tol
+
+    @property
+    def is_unit(self):
+        return np.abs(1.0 - np.linalg.norm(self.array)) < self.__tol
+
+    @property
+    def tol(self):
+        return self.__tol
--- python_quaternions/test/test_quaternion.py
+++ python_quaternions/test/test_quaternion.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 
-from python_quaternions.quaternion import Quaternion
+from python_quaternions.quaternion import Quaternion, UnitQuaternion
 
 
 def assert_array_equal(array_0, array_1, tol=0.0):
@@ -39,9 +39,9 @@ def test_quaternion_normalize():
     """Test that normalizing a quaternion with non-zero magnitude works."""
     # arrange
     w = 1.0
-    x = 1.0
+    x = -1.0
     y = 1.0
-    z = 1.0
+    z = -1.0
 
     q = Quaternion(w, x, y, z)
 
@@ -50,9 +50,9 @@ def test_quaternion_normalize():
 
     # assert
     assert q.w == 0.5
-    assert q.x == 0.5
+    assert q.x == -0.5
     assert q.y == 0.5
-    assert q.z == 0.5
+    assert q.z == -0.5
 
 
 def test_quaternion_normalize_zero_magnitude():
@@ -68,6 +68,73 @@ def test_quaternion_normalize_zero_magnitude():
 
     q = Quaternion(w, x, y, z)
 
+    err_msg = 'Cannot normalize quaternion with non-positive magnitude.'
+
     # act/assert
-    with pytest.raises(ValueError, match='Cannot normalize quaternion with magnitude less than 0'):
+    with pytest.raises(ValueError, match=err_msg):
         q.normalize()
+
+
+def test_unit_quaternion_zero_error_input():
+    # arrange
+    w = 1.0
+    x = 0.0
+    y = 0.0
+    z = 0.0
+
+    # act
+    q = UnitQuaternion(w, x, y, z)
+
+    # assert
+    assert q.w == 1.0
+    assert q.x == 0.0
+    assert q.y == 0.0
+    assert q.z == 0.0
+    assert q.is_unit
+    assert q.tol == 1e-5
+
+
+def test_unit_quaternion_zero_magnitude():
+    # arrange
+    w = 0.0
+    x = 0.0
+    y = 0.0
+    z = 0.0
+
+    err_msg = ('Cannot create UnitQuaternion from components with zero '
+               'magnitude.')
+
+    # act/assert
+    with pytest.raises(ValueError, match=err_msg):
+        q = UnitQuaternion(w, x, y, z)
+
+
+def test_unit_quaternion_within_tolerance_input():
+    # arrange
+    w = 1 - 1e-5  # within default tolerance of UnitQuaternion
+    x = 0.0
+    y = 0.0
+    z = 0.0
+
+    # act
+    q = UnitQuaternion(w, x, y, z)
+
+    # assert
+    assert q.w == 1.0
+    assert q.x == 0.0
+    assert q.y == 0.0
+    assert q.z == 0.0
+
+def test_unit_quaternion_out_of_tolerance_input():
+    # arrange
+    w = 1.1
+    x = 1.1
+    y = 1.1
+    z = 1.1
+
+    err_msg = ('The magnitude of the inputted components is greater than the '
+               'tolerance for rounding to 1.')
+
+    # act/assert
+    with pytest.raises(ValueError, match=err_msg):
+        q = UnitQuaternion(w, x, y, z)
