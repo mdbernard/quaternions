8139d8355a7c9e2f5e775ccc942b4241b49b5f54
Mike Bernard <mike.bernard@uconn.edu>
2021-08-04 18:51:00 -0700

    create quaternion_RT.py

---
+++ python_quaternions/quaternion_RT.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from quaternion import UnitQuaternion
+
+
+class Quaternion_RT(UnitQuaternion):
+    """
+    A right, transformational unit quaternion representing a passive (alias)
+    coordinate transformation.
+    """
+    pass
